bcc38b3421af1504fe8a89bf91a46bdac549ed1f
vincentdubois-animator <vince.dub6@outlook.fr>
2024-11-02 12:21:34 +0100

    Correction menu bouton

--- js/app.js
+++ js/app.js
@@ -26,12 +26,17 @@ function openMenu() {
     if (menu.classList.contains("active")) {
         menu.style.display = "flex"; // Affiche le menu
         document.querySelector("header .material-symbols-outlined").innerHTML = "close";
-    } else {
-        menu.style.display = "none"; // Cache le menu
-        document.querySelector("header .material-symbols-outlined").innerHTML = "menu";
-    }
+        closeMenu(); // Appelle closeMenu pour attacher les événements
+    } 
+    // A priori , le code ci-dessous ne sert pas car on passe jamais dedans 
+    // else {
+    //     menu.style.display = "none"; // Cache le menu
+    //     document.querySelector("header .material-symbols-outlined").innerHTML = "menu";
+    // }
 }
 
+
+function closeMenu() {
     // Fermer le menu quand un lien est cliqué
     document.querySelectorAll('.header-menu a').forEach(link => {
         link.addEventListener('click', (event) => {
@@ -43,3 +48,17 @@ function openMenu() {
             }
         });
     });
+
+}
+
+    // // Fermer le menu quand un lien est cliqué
+    // document.querySelectorAll('.header-menu a').forEach(link => {
+    //     link.addEventListener('click', (event) => {
+    //         const menu = document.querySelector(".header-menu");
+    //         if (menu.classList.contains("active")) {
+    //             menu.classList.remove("active");
+    //             menu.style.display = "none"; // Cache le menu
+    //             document.querySelector("header .material-symbols-outlined").innerHTML = "menu"; // Change l'icône
+    //         }
+    //     });
+    // });
